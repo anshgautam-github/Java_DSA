d091c85ea50e8c623b09857d5c3f632f896728d7
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2024-04-06 22:40:53 +0530

    Update GenerateSubsets.java

--- RECURSION/GenerateSubsets.java
+++ RECURSION/GenerateSubsets.java
@@ -4,4 +4,30 @@
                                                             /   \       / \
                                                           ""    "B"     "A" "AB"
                                                         / \     / \      / \     /  \
-                                                      ""  "C"  "B" "BC" "A" "AC" "AB" "ABC"    
+                                                      ""  "C"  "B" "BC" "A" "AC" "AB" "ABC"   
+                                                                  
+// LOGIC ->> Try to break a problem into smaller parts
+// here, we can take an empty string , and keep then we have 2 cases -> remaain as it as || (or) add a new charac
+
+public class GenerateSubsets {
+    
+  	
+	static void printSub(String str, String curr, int index)
+	{
+		if(index == str.length())
+		{
+			System.out.print(curr+" ");
+			return;
+		}
+
+		printSub(str, curr, index + 1);
+		printSub(str, curr+str.charAt(index), index + 1);
+	}
+    public static void main(String [] args) 
+    {
+    	String str = "ABC";
+    	
+    	printSub(str, "", 0);
+        
+    }
+}
