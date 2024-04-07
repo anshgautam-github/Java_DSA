b33567dd7a1a658cb167a2a1a50f285770f1a5a7
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2024-04-07 19:17:48 +0530

    Update SumOfDigitsUsingRecursion.java

--- RECURSION/SumOfDigitsUsingRecursion.java
+++ RECURSION/SumOfDigitsUsingRecursion.java
@@ -22,3 +22,14 @@ public class SumOfDigitsUsingRecursion {
     }
 
 }
+
+
+// return sumOfDigits(n/10) + n%10;: In this expression, sumOfDigits(n/10) recursively calculates the sum of digits in the number n/10 (i.e., removing the last digit), and then n%10 calculates the last digit of the number n. Finally, these two results are added together. This approach correctly sums the digits from right to left, similar to how you would read a number.
+
+// return n%10 + sumOfDigits(n/10);: In this expression, n%10 calculates the last digit of the number n, and then sumOfDigits(n/10) recursively calculates the sum of digits in the number n/10. Finally, these two results are added together. However, this approach calculates the sum of digits from left to right, which is not correct for this problem.
+
+// The second approach doesn't give the correct result because it doesn't sum the digits in the correct order. It calculates the sum from left to right, which means it starts adding digits from the most significant digit to the least significant digit, which doesn't correspond to the actual numeric value of the digits in the number.
+
+// Therefore, to correctly calculate the sum of digits in a number using recursion, you should use the first approach return sumOfDigits(n/10) + n%10;, where you first recursively calculate the sum of digits in the remaining number (n/10) and then add the last digit (n%10). This way, the digits are summed from right to left, which corresponds to the correct numeric value of the digits in the number.
+
+//in maths also we are do from right to left 
