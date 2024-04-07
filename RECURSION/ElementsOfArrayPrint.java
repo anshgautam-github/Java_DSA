e2885efe5b12552f7554200cdcfbcdf88cc401b6
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2024-04-07 19:13:04 +0530

    Create ElementsOfArrayPrint.java

---
+++ RECURSION/ElementsOfArrayPrint.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            int arr[] = new int[N];
+            String str[] = read.readLine().trim().split(" ");
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(str[i]);
+                
+            new Solution().printArrayRecursively(arr, N);
+            System.out.println();
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+
+
+    public static void printArrayRecursively(int arr[], int n)
+    {
+        // Print the array elements recursively
+            
+            if(n==0) return;
+            
+            //can be thought of Nto1 print ques of recusrion 
+            
+            System.out.print(arr[0]+" ");
+            printArrayRecursively(Arrays.copyOfRange(arr,1,n),n-1)  ;  
+    }   
+}
+
+
+
+
+//if instead of n , index was givien in ques then it would be easy ;
+//   public static void printArrayRecursively(int arr[], int index) {
+      
+      
+//       if(index==arr.length) return;
+      
+//       System.out.print(arr[index]+ " ");
+      
+//       printArrayRecursively(arr,index+1);
+//   }
