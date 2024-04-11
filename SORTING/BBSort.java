e77b227a908c7de4a351a01be078a127d443c21d
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2024-04-11 22:29:44 +0530

    Create BBSort.java

---
+++ SORTING/BBSort.java
@@ -0,0 +1,30 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class GFG {
+    
+    static void bubbleSort(int arr[], int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n - i - 1; j++){
+                if( arr[j] > arr[j + 1]){
+                    
+                    // swapping
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    
+                }
+            }
+        }
+    }
+    
+	public static void main (String[] args) {
+	    int a[] = {2, 1, 4, 3};
+	    bubbleSort(a, 4);
+	    
+	    for(int i = 0; i < 4; i++){
+	        System.out.print(a[i] + " ");
+	    }
+	}
+}
