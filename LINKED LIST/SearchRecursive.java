9e9abaaa40d232718ac56361b0b3f12add6791bd
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2024-04-17 17:06:45 +0530

    Create SearchRecursive.java

---
+++ LINKED LIST/SearchRecursive.java
@@ -0,0 +1,47 @@
+// Time Complexity: O(n)
+// Space Complexity: O(n)   ->> because (n+1) calls in the function call stack 
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node{
+        int data;
+        Node next;
+        Node(int x){
+            data=x;
+            next=null;
+        }
+    }
+  
+class SearchRecursive { 
+    
+    public static void main(String args[]) 
+    { 
+        Node head=new Node(10);
+    	head.next=new Node(20);
+    	head.next.next=new Node(30);
+    	printlist(head);
+    	System.out.println("Position of element in Linked List: "+search(head,20));
+    	
+    } 
+     
+    static int search(Node head, int x){
+        if(head==null)return -1;
+        if(head.data==x)return 1;
+        else{
+            int res=search(head.next,x);
+            if(res==-1)return -1;
+            else return res+1;
+        }
+    }
+
+  
+    public static void printlist(Node head){
+        Node curr=head;
+        while(curr!=null){
+        System.out.print(curr.data+" ");
+        curr=curr.next;
+    }System.out.println();
+    }
+} 
