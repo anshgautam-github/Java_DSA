3901ee78bc7e3fa84fc76cb3219fc44b0459c0f6
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2024-04-17 16:44:43 +0530

    Create TraverseIteration.java

---
+++ LINKED LIST/TraverseIteration.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node{
+        int data;
+        Node next;
+        Node(int x){
+            data=x;
+            next=null;
+        }
+    }
+  
+class TraverseIteration { 
+    
+    public static void main(String args[]) 
+    { 
+        Node head=new Node(10);
+    	head.next=new Node(20);
+    	head.next.next=new Node(30);
+    	head.next.next.next=new Node(40);
+    	printlist(head);
+    	
+    } 
+    
+    public static void printlist(Node head){
+        Node curr=head;
+        while(curr!=null){
+        System.out.print(curr.data+" ");
+        curr=curr.next;
+    }
+    }
+} 
