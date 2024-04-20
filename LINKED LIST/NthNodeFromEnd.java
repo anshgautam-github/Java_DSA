7a8d9ded9fa82cac51699c537eb49ec95f042cd5
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2024-04-20 22:14:30 +0530

    Update NthNodeFromEnd.java

--- LINKED LIST/NthNodeFromEnd.java
+++ LINKED LIST/NthNodeFromEnd.java
@@ -25,17 +25,35 @@ class NthNodeFromEnd {
     	printNthFromEnd(head,2);
     	
     } 
-    
+
+        //METHOD -> Length Method
+    // static void printNthFromEnd(Node head,int n){
+    //      int len=0;
+    //     for(Node curr=head;curr!=null;curr=curr.next)  //for finding the length of the LL
+    //         len++;
+    //     if(len<n)  
+    //         return;
+    //     Node curr=head;
+    //     for(int i=1;i<len-n+1;i++)
+    //         curr=curr.next;
+    //     System.out.print(curr.data);
+    // }
+
+
+        //METHOD -> 2 pointer method ->> 
     static void printNthFromEnd(Node head,int n){
-         int len=0;
-        for(Node curr=head;curr!=null;curr=curr.next)  //for finding the length of the LL
-            len++;
-        if(len<n)  
-            return;
-        Node curr=head;
-        for(int i=1;i<len-n+1;i++)
-            curr=curr.next;
-        System.out.print(curr.data);
+        if(head==null)return;
+        Node first=head;
+        for(int i=0;i<n;i++){
+            if(first==null)return;
+            first=first.next;
+        }
+        Node second=head;
+        while(first!=null){
+            second=second.next;
+            first=first.next;
+        }
+        System.out.print(second.data);
     }
     
     public static void printlist(Node head){
