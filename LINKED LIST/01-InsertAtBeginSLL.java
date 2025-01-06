79799ca470610f19dfc4254433188f6fcb3f28f3
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-01-06 19:06:46 +0530

    Update 01-InsertAtBeginSLL.java

--- LINKED LIST/01-InsertAtBeginSLL.java
+++ LINKED LIST/01-InsertAtBeginSLL.java
@@ -1,39 +1,80 @@
+
+// class Node {
+//     int val;
+//     Node next;
+
+//     Node(int val) {
+//         this.val = val;
+//         this.next = null;  
+//     }
+// }
+
+// public class LinkedList {
+    
+ 
+//     public void insertAtHead(Node head, int val) {
+//         Node newNode = new Node(val);
+//         newNode.next = head;
+//         head = newNode;
+//     }
+
+//     public void printList(Node head) {
+//         Node current = head;
+//         while (current != null) {
+//             System.out.print(current.val + " "); 
+//             current = current.next;             
+//         }  
+//     }
+
+//     public static void main(String[] args) {
+//         LinkedList list = new LinkedList();
+        
+//         Node head = null; 
+//         list.insertAtHead(head, 5);  // List: 5
+//         list.insertAtHead(head, 10); // List: 10 -> 5
+//         list.insertAtHead(head, 15); // List: 15 -> 10 -> 5
+
+//         list.printList(head);  // Output will be: 15 10 5
+//     }
+// }
+
 class Node {
-    int data;
+    int val;
     Node next;
-    Node(int data) {
-        this.data = data;
-        next = null;
-    }
-}
 
-class InsertAtBeginSLL {
-    Node head;
-    public void insertAtBegin(int data) {
-        Node newNode= new Node(data);
-        newNode.next=head;
-        head=newNode;
-      
-    }
-
-    public void printList() {
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + " ");
-            temp = temp.next;
-        }
+    Node(int val) {
+        this.val = val;
+        this.next = null;
     }
 }
 
 public class Main {
+    
+    public Node insertAtHead(Node head, int val) {
+        Node newNode = new Node(val);  
+        newNode.next = head;          
+        head = newNode;                
+        return head;                  
+    }
+
+    public void printList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println(); 
+    }
+
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.insertAtBegin(1);
-        list.insertAtBegin(2);
-        list.insertAtBegin(3);
-        list.insertAtBegin(4);
-        list.insertAtBegin(5);
-        System.out.print("Linked List: ");
-        list.printList();
+        Main list = new Main();
+        
+        Node head = new Node(10);  
+        
+        head = list.insertAtHead(head, 5); 
+        head = list.insertAtHead(head, 20); 
+        head = list.insertAtHead(head, 15); 
+
+        list.printList(head); 
     }
 }
