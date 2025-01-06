51ca2d565f0ed4ede4d268ac32ba9c7af4fc9fa8
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-01-06 23:32:27 +0530

    Update SumOfNodesOfLL.java

--- LINKED LIST/SumOfNodesOfLL.java
+++ LINKED LIST/SumOfNodesOfLL.java
@@ -1,14 +1,38 @@
+class Node {
+    int data;
+    Node next;
 
-class SumOfNodesOfLL
-{
-    public static int sumOfElements(Node head)
-    {
-        // your code here
-        int sum=0;
-        for(Node curr=head; curr!=null; curr=curr.next){
-            sum=sum+curr.data;
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class SumOfNodesOfLL {
+
+    
+    public int sumOfNodes(Node head) {
+        int sum = 0;
+        Node current = head;
+
+        while (current != null) {
+            sum += current.data; 
+            current = current.next; 
         }
+
         return sum;
-        
+    }
+
+    public static void main(String[] args) {
+        SumOfNodesOfLL list = new SumOfNodesOfLL();
+
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        int totalSum = list.sumOfNodes(head);
+        System.out.println(totalSum);
     }
 }
