0dd444cb0828142807c1d54425b3896c89829c24
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-01-10 12:25:37 +0530

    Update 05-DFS.java

--- GRAPH/05-DFS.java
+++ GRAPH/05-DFS.java
@@ -1,1 +1,56 @@
 
+import java.util.*;
+
+public class DFSExample {
+
+    public static void dfs(List<List<Integer>> graph, boolean[] visited, int src) {
+        List<Integer> neighbors = graph.get(src); 
+
+        for (int neighbor : neighbors) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true; 
+                System.out.println("Visited: " + neighbor);
+
+                // Recursively visit the neighbor
+                dfs(graph, visited, neighbor);
+            }
+        }
+    }
+
+    public static List<List<Integer>> constructGraph(int N, int M, int[][] edges) {
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        return graph;
+    }
+
+    public static void main(String[] args) {
+        int N = 5; 
+        int M = 6; 
+
+        int[][] edges = {
+            {0, 1},
+            {0, 2},
+            {1, 2},
+            {1, 3},
+            {3, 4},
+            {4, 0}
+        };
+        List<List<Integer>> graph = constructGraph(N, M, edges);
+
+        boolean[] visited = new boolean[N];
+        visited[0] = true;
+        dfs(graph, visited, 0);
+    }
+}
