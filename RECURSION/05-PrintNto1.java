ae9001dd7c2d8ffba0b4b267432a9b34f1d2efc6
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-01-15 23:45:01 +0530

    Update and rename PrintNto1.java to 05-PrintNto1.java

---
+++ RECURSION/05-PrintNto1.java
@@ -0,0 +1,19 @@
+//DESCENDING ORDER 
+
+public class PrintNto1 {
+
+  
+    public static void printNumbers(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        System.out.print(n + " "); 
+        printNumbers(n-1);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.print("Numbers from " + n + " to 1: ");
+        printNumbers(n);
+    }
+}
