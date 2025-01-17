05969529c4f3591646837dce869197b93019f6d2
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-01-17 18:27:50 +0530

    Create RotateMatrixAntiClock.java

---
+++ ARRAY/RotateMatrixAntiClock.java
@@ -0,0 +1,49 @@
+public class RotateMatrixAntiClock {
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void rotateMatrixAntiClockwise(int[][] matrix) {
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each column to get the final rotated matrix
+        for (int j = 0; j < n; j++) {
+            for (int i = 0, k = n - 1; i < k; i++, k--) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[k][j];
+                matrix[k][j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        rotateMatrixAntiClockwise(matrix);
+
+        System.out.println("Rotated Matrix (90 degrees anti-clockwise):");
+        printMatrix(matrix);
+    }
+}
