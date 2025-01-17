7000dc9750e7c7089ef3d7e12c0461e190b48d24
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-01-17 18:25:55 +0530

    Create RotateMatrixClock.java

---
+++ ARRAY/RotateMatrixClock.java
@@ -0,0 +1,52 @@
+//Rotate Matrix Clockwise 90 degree
+
+public class RotateMatrixClock {
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void rotateMatrixClockwise(int[][] matrix) {
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix 
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row to get the final rotated matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0, k = n - 1; j < k; j++, k--) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][k];
+                matrix[i][k] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+          int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+      
+        rotateMatrixClockwise(matrix);
+
+        System.out.println("Rotated Matrix (90 degrees clockwise):");
+        printMatrix(matrix);
+    }
+}
+
