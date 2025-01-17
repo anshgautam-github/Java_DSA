243226316d80e6ac6fcb37edb9bb26fe2b5bb3b9
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-01-17 14:59:10 +0530

    Create Transpose.java

---
+++ ARRAY/Transpose.java
@@ -0,0 +1,38 @@
+public class Transpose {
+
+  public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void transposeInPlace(int[][] matrix) {
+        int n = matrix.length; 
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        transposeInPlace(matrix);
+
+        System.out.println("Transposed Matrix:");
+        printMatrix(matrix);
+    }
+}
